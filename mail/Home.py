26fbb8f11e6fb63c002f288ec5045e8b86e8a722
xingniandage <3376867662@qq.com>
2022-12-04 23:34:42 +0800

    UI

--- mail/Home.py
+++ mail/Home.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 from tkinter.messagebox import showinfo
 from tkinter.ttk import *
 from PIL import Image, ImageTk
@@ -14,25 +15,36 @@ class HomeWindow(Tk):
         self.title("主页")
         self.geometry("900x640+180+80")
         self.resizable(0, 0)# 窗体大小不允许变，两个参数分别代表x轴和y轴
-        self["bg"] = "white"
+        self.canvas=Canvas(self,highlightthickness=0)
+        self.canvas.place(width=900,height=640)
+        bg=PhotoImage(file='img/bg3.png')
+        self.canvas.create_image(450,320,image=bg)
         self.ismanager=ismanager
         self.setup_UI()
-
+        self.mainloop()
     def setup_UI(self):
+        self.Style01 = Style()
+        self.Style01.configure("user.TLabel", font=("华文黑体", 20, "bold"), foreground="royalblue")
+        self.Style01.configure("TEntry", font=("华文黑体", 20, "bold"))
+        self.Style01.configure("TButton", font=("华文黑体", 20, "bold"), foreground="royalblue")
         if self.ismanager==1:
-            self.Button_check = Button(self, text="管理成员", width=20, command=self.ManageCounter)  ###绑定函数
+            self.Button_check = Button(self, text="管理成员", width=20, command=self.ManageCounter,style='TButton')  ###绑定函数
             self.Button_check.pack(expand="yes", padx=40, pady=20)
         else:
-            self.Button_write = Button(self, text="写邮件", width=20, command=self.WriteMail)  ###绑定函数
+            self.Button_write = Button(self, text="写邮件", width=20, command=self.WriteMail,style='user.TButton')  ###绑定函数
             self.Button_write.pack(expand="yes", padx=40, pady=20)
             self.Button_check = Button(self, text="查看邮件", width=20, command=self.CheckMail)  ###绑定函数
             self.Button_check.pack(expand="yes", padx=40, pady=20)
 
     def WriteMail(self):
+        self.destroy()
         writeMailwindow = WriteMail.writeMailWindow()#跳转到WriteMail函数
 
+
     def CheckMail(self):
+        self.destroy()
         checkMailWindow = CheckMail.CheckMailWindow()#跳转到CheckMail函数
     
     def ManageCounter(self):
+        self.destroy()
         manageMailWindow = Manage.manageMailWindow()#跳转到管理员界面
